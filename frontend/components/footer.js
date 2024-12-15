7e5e8b4d7bd9fb31a2815fc19e8661592d583bba
Jayashre <jaya2004kra@gmail.com>
2024-12-15 15:00:05 +0530

    added footer content

---
+++ frontend/components/footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+      <footer className="bg-[#8d0801ff] text-white py-1 px-6 text-center poppins">
+        <p>
+          Developed by <strong>Team Definox</strong> for{' '}
+          <a
+            href="https://your-hackathon-link.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#f4d58dff] hover:text-[#708d81ff]"
+          >
+            <strong className="space_grotesk">HACKSTORM: INTEL® AI PC EDITION</strong> 
+          </a>
+          {" "}Hackathon
+        </p>
+      </footer>
+    );
+  };
+  
+export default Footer;
+  
