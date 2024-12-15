357d0af8085ba9c4fecb36da10fa3f5a69fb1f7e
Jayashre <jaya2004kra@gmail.com>
2024-12-15 22:55:57 +0530

    added footer

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -1,21 +1,28 @@
 const Footer = () => {
-    return (
-      <footer className="bg-[#8d0801ff] text-white py-1 px-6 text-center poppins">
-        <p>
-          Developed by <strong>Team Definox</strong> for{' '}
-          <a
-            href="https://your-hackathon-link.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-[#f4d58dff] hover:text-[#708d81ff]"
-          >
-            <strong className="space_grotesk">HACKSTORM: INTEL® AI PC EDITION</strong> 
-          </a>
-          {" "}Hackathon
-        </p>
-      </footer>
-    );
-  };
-  
+  return (
+    <footer className="bg-[#8d0801ff] text-white py-1 px-6 text-center poppins">
+      <p>
+        Developed by{' '}
+        <a
+          href="/submission.pdf"
+          download
+          className="text-[#f4d58dff] hover:text-[#708d81ff]"
+        >
+          <strong>Team Definox</strong>
+        </a>{' '}
+        for{' '}
+        <a
+          href="https://www.hackerearth.com/challenges/hackathon/intel-hackathon/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-[#f4d58dff] hover:text-[#708d81ff]"
+        >
+          <strong className="space_grotesk">HACKSTORM: INTEL® AI PC EDITION</strong>
+        </a>{' '}
+        Hackathon
+      </p>
+    </footer>
+  );
+};
+
 export default Footer;
-  
