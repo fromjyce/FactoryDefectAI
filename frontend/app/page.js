7c7e5cd5a517599c21c4fe351b4b419c9df69ad5
Jayashre <jaya2004kra@gmail.com>
2024-12-15 15:18:05 +0530

    added prototype

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -5,6 +5,7 @@ import Landing from '@/components/landing';
 import Features from '@/components/features';
 import Footer from '@/components/footer';
 import Flowchart from '@/components/flowchart';
+import Prototype from '@/components/prototype';
 
 export default function Home() {
   const [mounted, setMounted] = useState(false);
@@ -19,6 +20,7 @@ export default function Home() {
       <Header />
       <Landing />
       <Features />
+      <Prototype />
       <Flowchart/>
       <Footer/>
     </div>
