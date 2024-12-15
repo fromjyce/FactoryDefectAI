fc970b6ea27d11c4b53b01cdab2e4682673aa750
Jayashre <jaya2004kra@gmail.com>
2024-12-15 14:44:50 +0530

    added intel name

--- frontend/components/landing.js
+++ frontend/components/landing.js
@@ -1,20 +1,33 @@
+import Image from 'next/image';
+
 const Landing = () => {
-    return (
-      <section
-        id="landing"
-        className="relative bg-cover bg-center h-96"
-        style={{ backgroundImage: 'url(/landing.jpg)' }}
-      >
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="relative flex items-center justify-center h-full text-center text-white">
-          <div>
-            <h1 className="text-5xl font-black space_grotesk">Optimizing Defect Detection</h1>
-            <p className="mt-4 text-2xl poppins font-bold">Leveraging AI for Industry 4.0 Manufacturing</p>
+  return (
+    <section
+      id="landing"
+      className="relative bg-cover bg-center h-96"
+      style={{ backgroundImage: 'url(/landing.jpg)' }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative flex items-center justify-center h-full text-center text-[#FDFEFF]">
+        <div>
+          <h1 className="text-5xl font-black space_grotesk">Optimizing Defect Detection</h1>
+          <p className="mt-4 text-2xl poppins font-bold">
+            Leveraging Intel® AI Stack for Industry 4.0 Manufacturing
+          </p>
+          <div className="mt-6 flex justify-center items-center gap-2 poppins font-semibold">
+            <span>Powered by</span>
+            <Image
+              src="/intel.png"
+              alt="Intel Logo"
+              width={100}
+              height={32}
+              priority
+            />
           </div>
         </div>
-      </section>
-    );
-  };
-  
-  export default Landing;
-  
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
