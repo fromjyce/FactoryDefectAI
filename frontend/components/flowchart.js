063c82c5c7275116bb766ea554eddbe10305d590
Jayashre <jaya2004kra@gmail.com>
2024-12-15 15:20:53 +0530

    added workflow

--- frontend/components/flowchart.js
+++ frontend/components/flowchart.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Flowchart = () => {
   return (
     <section id="flowchart" className="py-9 px-7 bg-[#f6dda4]">
-      <h2 className="text-4xl font-bold text-center space_grotesk text-[#001427ff]">Solution Flowchart</h2>
+      <h2 className="text-4xl font-bold text-center space_grotesk text-[#001427ff]">Workflow</h2>
       <div className="mt-8 text-center">
         <Image
           src="/flowchart.jpg"
