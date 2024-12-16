b7cd2c6ddc3f3eb42f679acda80a806052f8fe0e
Jayashre <jaya2004kra@gmail.com>
2024-12-17 00:26:52 +0530

    added highlighting effect

--- frontend/components/flowchart.js
+++ frontend/components/flowchart.js
@@ -4,13 +4,13 @@ const Flowchart = () => {
   return (
     <section id="flowchart" className="py-9 px-7 bg-[#f6dda4]">
       <h2 className="text-4xl font-bold text-center space_grotesk text-[#001427ff]">Workflow</h2>
-      <div className="mt-8 text-center">
+      <div className="mt-8 text-center ">
         <Image
           src="/flowchart.png"
           alt="Solution Flowchart"
           width={1200}
           height={600}
-          className="mx-auto rounded-lg shadow-lg p-4 bg-[#f4f4f4]"
+          className="mx-auto rounded-lg shadow-lg p-4 bg-[#f4f4f4] transform transition-all duration-300 hover:scale-105 hover:shadow-xl"
         />
       </div>
     </section>
